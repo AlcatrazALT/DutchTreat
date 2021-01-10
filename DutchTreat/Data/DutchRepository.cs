d667d13a86107a874badd4b258a409f9cf8f05ee
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-10 14:18:25 +0700

    feat: Add logging

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,6 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,24 +9,39 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext context;
+        private readonly ILogger<DutchRepository> logger;
 
-        public DutchRepository(DutchContext context)
+        public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
         {
             this.context = context;
+            this.logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return context.Products
-                .OrderBy(p => p.Title)
-                .ToList();
+            try
+            {
+                logger.LogInformation("Get All Products was called");
+                return context.Products.OrderBy(p => p.Title).ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
         {
-            return context.Products
-                .Where(p => p.Category == category)
-                .ToList();
+            try
+            {
+                return context.Products.Where(p => p.Category == category).ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get all products by category: {ex}");
+                return null;
+            }
         }
 
         public bool SaveAll()
