5f20a43a7e8bc7814ab08255134c9f3f48f1c71f
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-12 08:55:01 +0700

    feat: Add simple .ts files

---
+++ DutchTreat/wwwroot/ts/storecustomer.ts
@@ -0,0 +1,19 @@
+﻿class StoreCustomer {
+    public visits = 0
+    private ourName: string
+
+    constructor(private firstName: string, private lastName: string) {
+    }
+
+    public showName() {
+        alert(`${this.firstName} ${this.lastName}`)
+    }
+
+    set name(value) {
+        this.ourName
+    }
+
+    get name() {
+        return this.ourName;
+    }
+}
